bdbbd03140c33d088a4e2417f0dc9b0427188a04
SaveGex <Alekseew.zheka0207@gmail.com>
2024-06-16 17:22:41 +0300

    difine

--- lesson11/lesson11.cpp
+++ lesson11/lesson11.cpp
@@ -9,60 +9,30 @@
 //bonk2 = find(array.begin(), array.end(), bot1);
 //index_bot = distance(array.begin(), bonk2);
 
-
+#define POWS(a,b) pow(a,b)
+#define DIVIDE(a) ((a%2==0) ? "even":"odd")
+#define pos_neg(a) ((a>0)?"positive":"negative")
 
 using namespace std;
 
 
 
-typedef struct {
-
-    string name;
-
-    char gender;
-
-    double GPA;
-
-} Student;
-
-void Counter(Student* asd, int size) {
-    int countM = 0, countF = 0;
-    double scoreM = 0, scoreF = 0;
-    for (int i = 0; i < size; i++) {
-        if (asd[i].gender == 'M') {
-            countM++;
-            scoreM += asd[i].GPA;
-        }
-        if (asd[i].gender == 'F') {
-            countF++;
-            scoreF += asd[i].GPA;
-        }
-    }
-    float score1 = scoreF / countF;
-    float score2 = scoreM / countM;
-    if (score1 > score2) {
-        cout << "GPA boys big than girls";
-        cout << endl << score1 << "  |   " << score2;
-    }        
-
-    else {
-        cout << "GPA girls big than boys";
-        cout << endl << score1 << "  |   " << score2;
-
-    }
-}
-
-
 int main() {
-    Student asd[]{
-        {"name", 'M', 9.1},
-        {"rased", 'F', 1.2},
-        {"dfs", 'M', 1.9},
-        {"shfgd",'F', 10.6},
-        {"hjfikls",'M', 5.6}
+	int digit, digit2;
 
-    };
-    int size = sizeof(asd) / sizeof(asd[0]);
+	cout << "\nWrite num1: ";
+	cin >> digit;
 
-    Counter(asd, size);
+	cout << "\nWrite num2: ";
+	cin >> digit2;
+
+	cout << POWS(digit, digit2);
+
+	cout << endl << DIVIDE(digit) << endl << DIVIDE(digit2);
+
+	cout << endl << pos_neg(digit) << endl << pos_neg(digit2);
+
+
+
+	return 0;
 }
